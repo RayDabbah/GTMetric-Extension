c97aae3f752183a3884ab9149299ebf80446d92c
raydabbah <dabbahray@yahoo.com>
2017-08-13 09:31:10 -0400

    changed js file name and removed console.log

--- background.js
+++ background.js
@@ -2,7 +2,6 @@ button = document.querySelector('button');
 var activeUrl;
 chrome.tabs.query({active: true, currentWindow: true}, tabs => activeUrl = tabs[0].url);
 button.addEventListener('click', function(){
-    console.log(`2nd url: ${activeUrl}`);
     document.querySelector('input').value = activeUrl;
     document.querySelector('form').submit();
 })
